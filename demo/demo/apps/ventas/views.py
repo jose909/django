d0dc069373cbd3e255d3052805bf0f4424488331
jose rosales <rosaleayal19@hotmail.com>
2014-06-21 20:05:27 +0100

    agregando nuevo archivos

---
+++ demo/demo/apps/ventas/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def add_product_view(request):
+
+	return render_to_response('ventas/addProducto.html',context_instance=RequestContext(request))
